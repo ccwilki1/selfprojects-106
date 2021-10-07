7bae8239c9080a6dd40ae0f1a64243ae88ac80c2
ccwilki1 <ccwilki1@gmail.com>
2021-10-07 00:54:39 -0400

    adding new console proj

---
+++ C#_Fundamentials/c-train106/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace c_train106
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
