704f548427153a6d32cbe8e28150a3dedc78ebb6
ccwilki1 <ccwilki1@gmail.com>
2021-10-13 16:45:55 -0400

    10.13 commit

---
+++ C#_Fundamentials/resturant review/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace resturant_review
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ C#_Fundamentials/resturant review/RRModels/Restaurant.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace RRModels
+{
+    public class Restaurant
+    {
+        //This is a field
+        private string _city;
+
+        //This is a property that uses the field called _city
+        public string City
+        {
+            get { return _city; }
+            set 
+            {
+                //Main idea - this Regex will find me any number inside of my string
+                if (!Regex.IsMatch(value, @"^[A-Za-z .]+$"))
+                {
+                    //Will give the user an exception whenever you try to set the city field with a number
+                    throw new Exception("City can only hold letters!");
+                }
+                _city = value;
+            }
+        }
+
+        public string State { get; set; }
+        public string Name { get; set; }
+
+        public override string ToString()
+        {
+            return $"Name: {Name}\nState: {State}";
+        }
+
+    }
+}
